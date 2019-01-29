4ff411ee5b409eb739d51c874486fbe3b62480e4
karliv <kyzmikboy@mail.ru>
2019-01-29 18:31:09 +0300

    getting data from API Hacker News

--- src/App.js
+++ src/App.js
@@ -3,36 +3,56 @@ import './App.css'
 import Search from './components/Search'
 import UserList from './components/UserList'
 
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 export default class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            list: this.props.data,
-            searchTerm: ''
+            list: null,
+            searchTerm: 'redux'
+            //list: this.props.data,
+            //searchTerm: ''
         }
     };
 
     onDismiss = (id) => {
         const {list} = this.state;
-        const updatedList = list.filter((item) => item.objectID !== id);
+        const updatedList = list.hits.filter((item) => item.objectID !== id);
 
-        this.setState({list: updatedList})
+        this.setState({list: {...list, hits: updatedList}});
     };
 
     onSearchChange = (e) => {
-        this.setState({searchTerm: e.target.value})
+        this.setState({searchTerm: e.target.value});
     };
+
+    setSearchTopStories = (list) => {
+        this.setState({ list });
+    };
+
+    componentDidMount() {
+        const { searchTerm } = this.state;
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+            .then(response => response.json())
+            .then(list => this.setSearchTopStories(list))
+            .catch(error => error);
+    }
 
     render() {
         const {list, searchTerm} = this.state;
+
+        if (!list) { return null; }
 
         return (
             <div className="page">
                 <div className="interactions">
                     <Search value={searchTerm} onChange={this.onSearchChange}>Поиск</Search>
                 </div>
-                <UserList list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
+                <UserList list={list.hits} pattern={searchTerm} onDismiss={this.onDismiss}/>
             </div>
 
         );
